ad172ba36e2fdde11f45aee3278d845ba151ac1e
flashbax <jkiwtco@gmail.com>
2017-07-07 15:06:02 -0400

    additon of PHP into code

---
+++ process.php
@@ -0,0 +1,18 @@
+<?php
+//process.php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {//Check it is coming from a form
+    $u_firstname = $_POST["firstname"]; //set PHP variables like this so we can use them anywhere in code below
+    $u_lastname = $_POST["lastname"];
+    $u_phone = $_POST["phone"];
+    $u_referral = $_POST["referral"];
+    $u_lawyer = $_POST["lawyer"];
+    $u_type = $_POST["type"];
+    $u_dateofcontact = $_POST["dateofcontact"];
+    $u_formfilledby = $_POST["formfilledby"];
+    $u_comments = $_POST["comments"];
+    
+    //print output text
+    print "Hello " . $u_firstname . "!, we have received your message and phone number ". $u_phone;
+    print "We will contact you very soon!";
+}
+?>
