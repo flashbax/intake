bd5c85ca45893662fccc9aab36642ea46c35801b
flashbax <jkiwtco@gmail.com>
2017-07-14 13:53:50 -0400

    addition of validation to form elements

--- process.php
+++ process.php
@@ -1,16 +1,42 @@
 <?php
 //process.php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {//Check it is coming from a form
-    $u_firstname = $_POST["firstname"]; //set PHP variables like this so we can use them anywhere in code below
-    $u_lastname = $_POST["lastname"];
-    $u_phone = $_POST["phone"];
+    
+    $u_firstname = filter_var($_POST["firstname"], FILTER_SANITIZE_STRING); //set PHP variables like this so we can use them anywhere in code below
+    $u_lastname = filter_var($_POST["lastname"], FILTER_SANITIZE_STRING);
+    $u_phone = filter_var($_POST["phone"], FILTER_SANITIZE_STRING);
     $u_referral = $_POST["referral"];
     $u_lawyer = $_POST["lawyer"];
     $u_type = $_POST["type"];
-    $u_dateofcontact = $_POST["dateofcontact"];
-    $u_formfilledby = $_POST["formfilledby"];
-    $u_comments = $_POST["comments"];
+    $u_dateofcontact = filter_var($_POST["dateofcontact"], FILTER_SANITIZE_STRING);
+    $u_formfilledby = filter_var($_POST["formfilledby"], FILTER_SANITIZE_STRING);
+    $u_comments = filter_var($_POST["comments"], FILTER_SANITIZE_STRING);
     
+    if (empty($u_firstname)){
+        die("Please enter your first name");
+    }
+    
+    if (empty($u_lastname)){
+        die("Please enter your last name");
+    }
+        
+    if (empty($u_phone)){
+        die("Please enter phone number");
+    } 
+
+    if (empty($u_dateofcontact)){
+        die("Please enter the date of contact");
+    } 
+
+    if (empty($u_formfilledby)){
+        die("Please enter who took the call");
+    }
+
+    if (empty($u_comments)){
+        die("Please enter a comment");
+
+    }  
+
     //print output text
     print "Thank you " . $u_firstname . ". Your submission has been added to the database.<br /><br />";
     
